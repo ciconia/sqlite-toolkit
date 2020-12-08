e07c63842d9d52356f9fe5ee5248762b1b589a39
Sharon Rosner <sharon@noteflakes.com>
2020-12-08 19:21:42 +0000

    Implement setex, expire, incr, incrby

--- lib/sqlite-toolkit/red.rb
+++ lib/sqlite-toolkit/red.rb
@@ -20,6 +20,14 @@ module SQLiteToolkit
       )
     end
 
+    def setex(key, ttl, value)
+      stamp = Time.now.to_f + ttl
+      query(
+        "insert into red_map (key, value, expire_stamp) values(?, ?, ?) on conflict(key) do update set value = excluded.value, expire_stamp = excluded.expire_stamp",
+        [key, value.to_s, stamp]
+      )
+    end
+
     def get(key)
       get_first_value("select value from red_map where key = ?", [key])
     end
@@ -34,6 +42,33 @@ module SQLiteToolkit
 
     def keys
       query("select key from red_map").map { |r| r['key'] }
+    end
+
+    def delete_expired_keys
+      stamp = Time.now.to_f
+      query("delete from red_map where expire_stamp <= ?", [stamp])
+    end
+
+    def expire(key, ttl)
+      stamp = Time.now.to_f + ttl
+      query(
+        "update red_map set expire_stamp = ? where key = ?",
+        [stamp, key]
+      )
+    end
+
+    def incr(key)
+      query(
+        "insert into red_map (key, value) values(?, 1) on conflict(key) do update set value = cast(value as int) + 1",
+        [key]
+      )
+    end
+
+    def incrby(key, delta)
+      query(
+        "insert into red_map (key, value) values(?, ?) on conflict(key) do update set value = cast(value as int) + ?",
+        [key, delta, delta]
+      )
     end
   end
 end
--- test/test_red.rb
+++ test/test_red.rb
@@ -7,9 +7,6 @@ class KeyValueTest < Minitest::Test
     @db = SQLiteToolkit::RedDatabase.new('')
   end
 
-  def teardown
-  end
-
   def test_get_set
     assert_nil @db.get('foo')
     
@@ -62,5 +59,81 @@ class KeyValueTest < Minitest::Test
 
     @db.del('bar')
     assert_equal [], @db.keys
+  end
+
+  def test_setex
+    @db.setex('foo', 0.01, 42)
+    assert_equal '42', @db.get('foo')
+
+    sleep 0.005
+    @db.delete_expired_keys
+    assert_equal '42', @db.get('foo')
+    assert_equal ['foo'], @db.keys
+
+    sleep 0.006
+    @db.delete_expired_keys
+    assert_nil @db.get('foo')
+    assert_equal [], @db.keys
+
+
+    @db.setex('foo', 0.01, 42)
+    assert_equal '42', @db.get('foo')
+
+    sleep 0.005
+    @db.delete_expired_keys
+    assert_equal '42', @db.get('foo')
+    assert_equal ['foo'], @db.keys
+    
+    @db.setex('foo', 0.01, 43)
+    assert_equal '43', @db.get('foo')
+    assert_equal ['foo'], @db.keys
+
+    sleep 0.005
+    @db.delete_expired_keys
+    assert_equal '43', @db.get('foo')
+    assert_equal ['foo'], @db.keys
+
+    sleep 0.006
+    @db.delete_expired_keys
+    assert_nil @db.get('foo')
+    assert_equal [], @db.keys
+  end
+
+  def test_expire
+    @db.set('foo', 42)
+    @db.expire('foo', 0.01)
+
+    sleep 0.005
+    @db.delete_expired_keys
+    assert_equal '42', @db.get('foo')
+    assert_equal ['foo'], @db.keys
+
+    sleep 0.006
+    @db.delete_expired_keys
+    assert_nil @db.get('foo')
+    assert_equal [], @db.keys
+  end
+
+  def test_incr
+    @db.incr('foo')
+    assert_equal '1', @db.get('foo')
+    
+    @db.incr('foo')
+    assert_equal '2', @db.get('foo')
+
+    @db.del('foo')
+    @db.incr('foo')
+    assert_equal '1', @db.get('foo')
+  end
+
+  def test_incrby
+    @db.incrby('foo', 3)
+    assert_equal '3', @db.get('foo')
+    
+    @db.incrby('foo', -42)
+    assert_equal '-39', @db.get('foo')
+
+    @db.incrby('foo', 43)
+    assert_equal '4', @db.get('foo')
   end
 end
